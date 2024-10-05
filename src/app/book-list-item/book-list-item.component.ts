262bc036f01381e0bfd2e8012c5ad971bbf7f232
Riddhi Savsani <w0862829@myscc.ca>
2024-10-04 20:05:45 -0400

    Created Obserable Method and Subscribe it.

--- src/app/book-list-item/book-list-item.component.ts
+++ src/app/book-list-item/book-list-item.component.ts
@@ -2,15 +2,28 @@ import { Component, Input } from '@angular/core';
 import { Book } from "../models/book";
 import { CommonModule } from "@angular/common";
 import { NgOptimizedImage } from '@angular/common';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-book-list-item',
   standalone: true,
   imports: [CommonModule, NgOptimizedImage],
   templateUrl: './book-list-item.component.html',
-  styleUrls: ['./book-list-item.component.css'] // Corrected 'styleUrl' to 'styleUrls'
+  styleUrls: ['./book-list-item.component.css']
 })
 export class BookListItemComponent {
-  @Input() contentItem!: Book;  // The input property that accepts a Book object
-  @Input() isEven?: boolean;     // Optional input to determine if the item is even
+  @Input() contentItem!: Book;
+  @Input() isEven?: boolean;
+  bookList: Book[] = [];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.bookService.getBooksObservable().subscribe({
+      next: (data: Book[]) => this.bookList = data,
+      error:err => console.error("Error fetching Books",
+        err),
+      complete:() => console.log("Book data fetch complete!")
+    });
+  }
 }
