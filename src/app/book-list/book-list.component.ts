d30bbeb5d6ad34b75bd3dd95fc832c5ddc7eb5c0
Riddhi Savsani <w0862829@myscc.ca>
2024-11-20 10:34:52 -0500

    Created HoverHighlight directive to highlight buttons

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -6,11 +6,12 @@ import { BookService } from '../services/book.service';
 import { Router, RouterLink } from "@angular/router";
 import {BookDescriptionPipe} from "../pipes/book-description.pipe";
 import {AvailabilityStylePipe} from "../pipes/availability-style.pipe";
+import {HoverHighlightDirective} from "../directives/hover-highlight.directive";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule, BookListItemComponent, NgOptimizedImage, RouterLink, BookDescriptionPipe, AvailabilityStylePipe], // Import the child component
+  imports: [CommonModule, BookListItemComponent, NgOptimizedImage, RouterLink, BookDescriptionPipe, AvailabilityStylePipe, HoverHighlightDirective], // Import the child component
   templateUrl:'./book-list.component.html',
   styleUrls: ['./book-list.component.css'], // Correct the styleUrls property name
   encapsulation: ViewEncapsulation.None
