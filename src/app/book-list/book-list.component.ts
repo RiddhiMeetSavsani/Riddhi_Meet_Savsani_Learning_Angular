17edba36eed3e3674bfd6cfb0c7f2e99661e3402
Riddhi Savsani <w0862829@myscc.ca>
2024-12-02 20:28:05 -0500

    Assignment-10 completed

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -14,7 +14,7 @@ import { MatPaginatorModule} from "@angular/material/paginator";
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { ViewChild, AfterViewInit } from '@angular/core';
-import {MatTooltip, MatTooltipModule} from "@angular/material/tooltip";
+import {MatTooltipModule} from "@angular/material/tooltip";
 @Component({
   selector: 'app-book-list',
   standalone: true,
